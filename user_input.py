9796e36692599ecf715beafcd143b2639ca04f71
Enigma28540 <sekwat.lemi@gmail.com>
2024-02-27 04:03:22 +0300

    First Commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('Enter your name: ')
+age = input('How old are you?')
+location = input('Enter your current location: ')
+print('Hello ' + name + ' you are ' + age +' years old and live in '+ location)
